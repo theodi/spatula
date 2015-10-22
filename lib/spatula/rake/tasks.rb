d816fa5c56cd05a009e7c3a892341b31c81f5f42
pikesley <github@orgraphone.org>
2015-10-22 16:23:28 +0100

    Use the gem

--- lib/spatula/rake/tasks.rb
+++ lib/spatula/rake/tasks.rb
@@ -1,38 +1,14 @@
-def databases
-  require 'yaml'
-  YAML.load(File.read('.kitchen.yml'))['suites'].
-    map { |s| begin s['attributes']['mysql']['password'] rescue nil end }.uniq.compact
-end
-
-def there_are_databases
-  databases.any?
-end
+require 'spatula'
 
 desc 'Create database'
 task :crebas do
-  databases.each do |database|
-    puts 'Creating database... '
-    sh "mysql -u root -e 'create database if not exists #{database}';"
-    sh "mysql -u root -e 'grant all on #{database}.* to \"#{database}\" identified by \"#{database}\";'"
-    sh "mysql -u root -e 'flush privileges;'"
-  end
+  Spatula::create '.kitchen.yml'
 end
 
 desc 'Start local MySQL server'
 task :start_mysql do
-  if there_are_databases
-    print 'Starting MySQL... '
-    sh 'mysql.server start' do |ok, res|
-      if ! ok
-        puts '********************************************************'
-        puts 'FAIL!'
-        puts 'Are you sure MySQL is installed?'
-        puts 'Make sure `mysql.server start` works before trying again'
-        puts '********************************************************'
-      else
-        sleep 5
-      end
-    end
+  if Spatula::any_dbs? '.kitchen.yml'
+    Spatula::start_mysql
   end
 end
 
